ae8c36aa59d9741fb4f764f460f2f3553a22137e
Yunsuyeong <98012833+Yunsuyeong@users.noreply.github.com>
2022-03-12 00:49:19 +0900

    2022-03-11 (사용자 간 메시지 기능 구현)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,37 @@
-alert("HI!");
+const Nickform = document.querySelector("#nick");
+const Messageform = document.querySelector("#message");
+const Messagelist = document.querySelector("ul");
+const Frontsocket = new WebSocket(`ws://${window.location.host}`);
+
+function Makemessage(type, payload){
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
+Frontsocket.addEventListener("open", function(){
+    console.log("Connected to server");
+});
+
+Frontsocket.addEventListener("message", function(message){
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    Messagelist.append(li);
+});
+
+Frontsocket.addEventListener("close", function(){
+    console.log("Disconnected from server");
+});
+
+Messageform.addEventListener("submit", function(event){
+    event.preventDefault();
+    const input = Messageform.querySelector("input");
+    Frontsocket.send(Makemessage("new_message", input.value));
+    input.value = "";
+});
+
+Nickform.addEventListener("submit", function(event){
+    event.preventDefault();
+    const input = Nickform.querySelector("input");
+    Frontsocket.send(Makemessage("nickname", input.value));
+    input.value="";
+});
--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
-const express = require('express');
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+import { Socket } from "dgram";
 
 const app = express();
 
 app.set("view engine", "pug");
-
 app.set("views", __dirname + "/views");
-
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function(request, response){
@@ -16,4 +17,33 @@ app.get("/*", function(request, response){
     response.redirect("/");
 });
 
-app.listen(3000);
+const handleListen = function(){
+    console.log("Listening on http://localhost:3000");
+};
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const Backsockets=[];
+
+wss.on("connection", function(Backsocket){
+    Backsockets.push(Backsocket);
+    Backsocket["nickname"] = "Anon";
+    console.log("Connected to browser");
+    Backsocket.on("close", function(){
+        console.log("Disconnected from browser");
+    });
+    Backsocket.on("message", function(message){
+        const ParsedMessage = JSON.parse(message);
+        switch(ParsedMessage.type){
+            case "new_message":
+                Backsockets.forEach(function(Asocket){
+                    Asocket.send(`${Backsocket.nickname}: ${ParsedMessage.payload}`);
+                });
+            case "nickname":
+                Backsocket["nickname"] = ParsedMessage.payload;
+        }
+    });
+});
+
+server.listen(3000, handleListen);
